2b8f5e052a6732766e95d5d64a829eab820aafe4
AmiAPatel <amip.06@gmail.com>
2019-10-30 00:01:10 -0400

    Added variables for aks details

--- pod-service-example/variables.tf
+++ pod-service-example/variables.tf
@@ -1,3 +1,11 @@
+## Resource group variables ##
+variable resource_group_name {}
+
+## AKS kubernetes cluster variables ##
+variable cluster_name {
+  default = "aks-poc"
+}
+
 variable "namespace" {
   default = "aks-demo"
 }
